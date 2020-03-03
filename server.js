f6e1a805cff821e589b7aa6c91ef791884f4ecc9
Sylwia <sylwia_24_24@wp.pl>
2020-03-03 18:20:09 +0100

    Add hbs, dark site

--- server.js
+++ server.js
@@ -1,26 +1,40 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('express-handlebars');
 
 const app = express();
 
+app.engine('.hbs', hbs());
+app.set('view engine', '.hbs');
+
+
+app.use(express.static(path.join(__dirname + '/public')));
+
 app.get('/', (req, res) => {
-  res.sendFile(path.join(_dirname + './views/index.html'));
+  res.render('index' );
 });
 
 app.get('/about', (req, res) => {
-  res.sendFile(path.join(__dirname + '/views/about.html'));
+  res.render('about', { layout: 'dark' });
 });
 
 app.get('/contact', (req, res) => {
-  res.sendFile(path.join(__dirname + '/views/contact.html'));
+  res.render('contact');
 });
 
 app.get('/info', (req, res) => {
-  res.sendFile(path.join(__dirname + '/views/info.html'));
+  res.render('info');
 });
 
 app.get('/history', (req, res) => {
-  res.sendFile(path.join(__dirname + '/views/history.html'));
+  res.render('history');
+});
+app.get('/hello/:name', (req, res) => {
+  res.render('hello', { name: req.params.name });
+});
+
+app.use((req, res) => {
+  res.status(404).send('404 not found...');
 });
 
 app.listen(8000, () => {
